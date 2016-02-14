3d6249728f64431fd033f876e872be7214382b87
jianfengs <Jianfeng.Sun@fox.com>
2016-02-13 16:24:59 -0800

    try

--- prerun.bat
+++ prerun.bat
@@ -1,1 +1,1 @@
-msiexec.exe /i "https://github.com/jianfengs/sauceLabsPOC/blob/master/AsperaConnect-ML-3.6.5.119227.msi?raw=true"  WIX_APP_FOLDER=WixPerMachineFolder ALLUSERS=0 REBOOT=REALLYSUPPRESS /qn
+msiexec.exe /i "https://github.com/jianfengs/sauceLabsPOC/blob/master/AsperaConnect-ML-3.6.5.119227.msi?raw=true"  WIX_APP_FOLDER=WixPerMachineFolder ALLUSERS=0 /qn
