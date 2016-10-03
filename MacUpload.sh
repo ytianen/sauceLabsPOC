8d4c429a662a3e2b12e4f2619a177292df4361a7
Shuang Fan <Shuang.Fan@fox.com>
2016-10-03 13:53:46 -0700

    try mac upload 1

--- MacUpload.sh
+++ MacUpload.sh
@@ -4,23 +4,24 @@ sudo hdiutil attach  http://download.asperasoft.com/download/sw/connect/3.6.6/As
 sudo installer -package /Volumes/Aspera\ Connect\ Installer/AsperaConnectInstaller.pkg -target /
 
 sudo hdiutil detach /Volumes/Aspera\ Connect\ Installer/
-#osascript <<EOD
-#    on run
-#        tell application "Aspera Connect"
-#	        activate
-#            set myFile to do shell script "echo $(<`pwd`/src/main/resources/filename.txt)"
-#	        tell window 1
-#		        tell application "System Events"
-#			        keystroke "g" using {command down, shift down}
-#			        delay 2
-#			        keystroke myFile
-#			        delay 1
-#			        keystroke return
-#			        delay 2
-#			        keystroke return
-#			        delay 2
-#		        end tell
-#	        end tell
-#        end tell
-#    end run
-#EOD
+osascript <<EOD
+    on run
+    delay 60
+        tell application "Aspera Connect"
+	        activate
+            set myFile to do shell script "echo $(<`pwd`/src/main/resources/filename.txt)"
+	        tell window 1
+		        tell application "System Events"
+			        keystroke "g" using {command down, shift down}
+			        delay 2
+			        keystroke myFile
+			        delay 1
+			        keystroke return
+			        delay 2
+			        keystroke return
+			        delay 2
+		        end tell
+	        end tell
+        end tell
+    end run
+EOD
