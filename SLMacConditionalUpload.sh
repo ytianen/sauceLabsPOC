3fb46869fe3a3e1fee5d19bca49e9e51f0218d36
Shuang Fan <Shuang.Fan@fox.com>
2017-02-02 15:43:05 -0800

    Mac uploads script

--- SLMacConditionalUpload.sh
+++ SLMacConditionalUpload.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-open /
 hdiutil attach  http://download.asperasoft.com/download/sw/connect/3.6.6/AsperaConnectInstaller-3.6.6.119698.dmg
 
-sudo installer -package /Volumes/Aspera\ Connect\ Installer/AsperaConnectInstaller.pkg -target /
+#sudo installer -package /Volumes/Aspera\ Connect\ Installer/AsperaConnectInstaller.pkg -target /
 
 #sudo hdiutil detach /Volumes/Aspera\ Connect\ Installer/
 
 
 
 MyValue1=$1
-MyValue2=$2
+#MyValue2=$2
 
 if ["$#" = "4"]
 then
