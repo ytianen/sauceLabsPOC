0da51b8c33ef7056aef08166abae797d26527ae3
Shuang Fan <Shuang.Fan@fox.com>
2017-02-02 14:18:07 -0800

    Mac uploads script

--- SLMacConditionalUpload.sh
+++ SLMacConditionalUpload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+open /
 sudo hdiutil attach  http://download.asperasoft.com/download/sw/connect/3.6.6/AsperaConnectInstaller-3.6.6.119698.dmg
 
 sudo installer -package /Volumes/Aspera\ Connect\ Installer/AsperaConnectInstaller.pkg -target /
