a7fdc4cf84a11c252ba147c8121c090df356b283
Shuang Fan <Shuang.Fan@fox.com>
2017-02-10 16:01:56 -0800

    aspera connect in mac

--- installAsperaInMac.sh
+++ installAsperaInMac.sh
@@ -8,7 +8,7 @@ open /Volumes/Aspera\ Connect\ Installer/AsperaConnectInstaller.pkg
 osascript <<EOD
     on run
 #        tell application "Installer" to activate
-#            tell application "System Events"
+           tell application "System Events"
                 tell process "Installer"
                 repeat until exists button "Continue" of window 1
                 end repeat
@@ -23,14 +23,14 @@ osascript <<EOD
 	            repeat until exists button "Install" of window 1
 	            end repeat
 	            click button "Install" of window 1
-	            repeat until exists button "Close" of window 1
+#	            repeat until exists button "Close Application to Install" of window 1
 	            end repeat
-#	            delay 30
 #	            click button "Close Application to Install" of sheet 1 of window 1
-#	            delay 2
+                repeat until exists button "Close" of window 1
+	            end repeat
 	            click button "Close" of window 1
                 end tell
-#            end tell
+         end tell
 #        end tell
     end run
 EOD
