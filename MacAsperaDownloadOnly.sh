ad192a0971151c38dbc9e6f230a261ed6caad83a
iano <ian.oroceo@fox.com>
2017-07-26 13:26:55 -0700

    adding aspera download only for Mac. to be used by portals

---
+++ MacAsperaDownloadOnly.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo hdiutil attach  http://download.asperasoft.com/download/sw/connect/3.6.6/AsperaConnectInstaller-3.6.6.119698.dmg
+
+sudo installer -package /Volumes/Aspera\ Connect\ Installer/AsperaConnectInstaller.pkg -target /
+
+sudo hdiutil detach /Volumes/Aspera\ Connect\ Installer/
+
+
+        tell application "Aspera Connect"
+	        activate
+	        tell application "System Events"
+	            tell process "Aspera Connect"
+			        repeat until exists of button 1 of window 1
+			        delay 1
+			        end repeat
+			        delay 2
+			        keystroke return
+#			        click button "Allow"
+                end tell
+            end tell
+        end tell
