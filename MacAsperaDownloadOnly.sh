4c97beacbc7e9e31acc12cc2bac2755c8d53757a
iano <ian.oroceo@fox.com>
2017-08-04 08:08:22 -0700

    adding remember selection

--- MacAsperaDownloadOnly.sh
+++ MacAsperaDownloadOnly.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-#comments-start
-sudo hdiutil attach  http://download.asperasoft.com/download/sw/connect/3.6.6/AsperaConnectInstaller-3.6.6.119698.dmg
-
-sudo installer -package /Volumes/Aspera\ Connect\ Installer/AsperaConnectInstaller.pkg -target /
-
-sudo hdiutil detach /Volumes/Aspera\ Connect\ Installer/
-#comments-end
+# sudo hdiutil attach  http://download.asperasoft.com/download/sw/connect/3.6.6/AsperaConnectInstaller-3.6.6.119698.dmg
+# sudo installer -package /Volumes/Aspera\ Connect\ Installer/AsperaConnectInstaller.pkg -target /
+# sudo hdiutil detach /Volumes/Aspera\ Connect\ Installer/
 
 
         tell application "Aspera Connect"
