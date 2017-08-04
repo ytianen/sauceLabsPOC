4bea4cecb16fa9e76749d2dc3d23523cf8c50da4
iano <ian.oroceo@fox.com>
2017-08-04 08:59:58 -0700

    updating applescript for download for portals

--- MacAsperaDownloadOnly.sh
+++ MacAsperaDownloadOnly.sh
@@ -3,13 +3,15 @@
 # sudo installer -package /Volumes/Aspera\ Connect\ Installer/AsperaConnectInstaller.pkg -target /
 # sudo hdiutil detach /Volumes/Aspera\ Connect\ Installer/
 
-
-tell application "System Events" to tell process "Aspera Connect"
-    repeat until exists of button 1 of window 1
-        delay 1
-    end repeat
-        delay 2
-    click checkbox "Use my choice" of window 1
-        delay 2
-    keystroke return
+tell application "Aspera Connect"
+    activate
+        tell application "System Events" to tell process "Aspera Connect"
+            repeat until exists of button 1 of window 1
+                delay 1
+            end repeat
+                delay 2
+            click checkbox "Use my choice" of window 1
+                delay 2
+            keystroke return
+        end tell
 end tell
