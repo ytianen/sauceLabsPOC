ba13eaa09cbef0c314ce41cb744b9f50334b0533
iano <ian.oroceo@fox.com>
2017-08-04 14:28:10 -0700

    updating applescript for download for portals

--- MacAsperaDownloadOnly.sh
+++ MacAsperaDownloadOnly.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# sudo hdiutil attach  http://download.asperasoft.com/download/sw/connect/3.6.6/AsperaConnectInstaller-3.6.6.119698.dmg
-# sudo installer -package /Volumes/Aspera\ Connect\ Installer/AsperaConnectInstaller.pkg -target /
-# sudo hdiutil detach /Volumes/Aspera\ Connect\ Installer/
+ sudo hdiutil attach  http://download.asperasoft.com/download/sw/connect/3.6.6/AsperaConnectInstaller-3.6.6.119698.dmg
+ sudo installer -package /Volumes/Aspera\ Connect\ Installer/AsperaConnectInstaller.pkg -target /
+ sudo hdiutil detach /Volumes/Aspera\ Connect\ Installer/
 
 tell application "Aspera Connect"
 	activate
