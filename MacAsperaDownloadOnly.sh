b31ebe4715546a074876bfd98f18a79808563d2a
iano <ian.oroceo@fox.com>
2017-08-04 07:53:35 -0700

    adding remember selection

--- MacAsperaDownloadOnly.sh
+++ MacAsperaDownloadOnly.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
+#comments-start
 sudo hdiutil attach  http://download.asperasoft.com/download/sw/connect/3.6.6/AsperaConnectInstaller-3.6.6.119698.dmg
 
 sudo installer -package /Volumes/Aspera\ Connect\ Installer/AsperaConnectInstaller.pkg -target /
 
 sudo hdiutil detach /Volumes/Aspera\ Connect\ Installer/
+#comments-end
 
 
         tell application "Aspera Connect"
 	        activate
 	        tell application "System Events"
 	            tell process "Aspera Connect"
-			        repeat until exists of button 1 of window 1
-			        delay 1
-			        end repeat
-			        delay 2
-			        keystroke return
-#			        click button "Allow"
+	                tell checkbox 1 to if value is 0 then click
+			            repeat until exists of button 1 of window 1
+			            delay 1
+			            end repeat
+			            delay 2
+			            keystroke return
+#			            click button "Allow"
+                    end tell
                 end tell
             end tell
         end tell
