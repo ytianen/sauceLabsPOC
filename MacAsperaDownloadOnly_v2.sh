81ecaeea119c81907ecc75e0890aa9bfaefae7e0
iano <ian.oroceo@fox.com>
2017-08-09 09:15:28 -0700

    adding version for Mac not using keystroke

---
+++ MacAsperaDownloadOnly_v2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# sudo hdiutil attach  http://download.asperasoft.com/download/sw/connect/3.6.6/AsperaConnectInstaller-3.6.6.119698.dmg
+# sudo installer -package /Volumes/Aspera\ Connect\ Installer/AsperaConnectInstaller.pkg -target /
+# sudo hdiutil detach /Volumes/Aspera\ Connect\ Installer/
+
+tell application "Aspera Connect"
+	activate
+	tell application "System Events"
+		tell process "Aspera Connect"
+			repeat until exists button "Allow" of window 1
+				delay 1
+			end repeat
+			delay 1
+			click checkbox of window 1
+			delay 1
+			click button "Allow" of window 1
+		end tell
+	end tell
+end tell
