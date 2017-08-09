b096c7c9bb85c8a03a6ce187a565a22afb63f218
iano <ian.oroceo@fox.com>
2017-08-09 10:54:05 -0700

    updating download mac applescript. adding log

--- MacAsperaDownloadOnly.sh
+++ MacAsperaDownloadOnly.sh
@@ -1,41 +1,4 @@
 #!/bin/bash
-# sudo hdiutil attach  http://download.asperasoft.com/download/sw/connect/3.6.6/AsperaConnectInstaller-3.6.6.119698.dmg
-# sudo installer -package /Volumes/Aspera\ Connect\ Installer/AsperaConnectInstaller.pkg -target /
-# sudo hdiutil detach /Volumes/Aspera\ Connect\ Installer/
-
-(*tell application "Aspera Connect"
-	activate
-	tell application "System Events"
-		tell process "Aspera Connect"
-			repeat until exists button "Allow" of window 1
-				delay 1
-			end repeat
-			delay 1
-			click checkbox of window 1
-			delay 1
-			click button "Allow" of window 1
-		end tell
-	end tell
-end tell*)
-
-(*tell application "Aspera Connect"
-	activate
-	tell application "System Events"
-		tell process "Aspera Connect"
-			repeat until exists of button 1 of window 1
-				delay 1
-			end repeat
-			log
-			delay 2
-			keystroke return
-			delay 2
-			keystroke return
-			log
-		end tell
-	end tell
-end tell*)
-
-#!/bin/bash
 osascript <<EOD
     tell application "Aspera Connect"
             activate
