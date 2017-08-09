299c3e3f2278bcce200bf1017ca87f747d42402d
iano <ian.oroceo@fox.com>
2017-08-09 09:36:19 -0700

    updating download mac applescript

--- MacAsperaDownloadOnly.sh
+++ MacAsperaDownloadOnly.sh
@@ -19,15 +19,15 @@
 end tell*)
 
 tell application "Aspera Connect"
-	activate
-	tell application "System Events"
-		tell process "Aspera Connect"
-			repeat until exists button "Allow" of window 1
-				delay 1
-			end repeat
-			delay 3
-			keystroke return
-		end tell
-	end tell
-end tell
+            activate
+            tell application "System Events"
+                tell process "Aspera Connect"
+                    repeat until exists of button 1 of window 1
+                    delay 1
+                    end repeat
+                    delay 2
+                    keystroke return
+                end tell
+            end tell
+        end tell
 
