ee506eae757f2e90b159e17c2bb88b4405811f75
iano <ian.oroceo@fox.com>
2017-08-09 11:39:44 -0700

    updating download mac applescript. adding log

--- MacAsperaDownloadOnly.sh
+++ MacAsperaDownloadOnly.sh
@@ -7,17 +7,21 @@ osascript <<EOD
                     repeat until exists of button 1 of window 1
                     delay 1
                     end repeat
+                    log
                     delay 2
                     keystroke return
                     delay 2
+                    log
                     keystroke return
                 end tell
+                log
             end tell
     end tell
     tell application "Terminal"
         activate
         tell application "System Events" to keystroke "n" using {command down}
         end tell
+        log
     end tell
 EOD
 
