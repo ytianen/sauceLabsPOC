6c322ce77c3e97d562934efa7f19e6a87100513f
iano <ian.oroceo@fox.com>
2017-08-10 13:36:19 -0700

    updating download mac applescript. adding log

--- MacAsperaDownloadOnly_v2.sh
+++ MacAsperaDownloadOnly_v2.sh
@@ -2,7 +2,7 @@
 # sudo hdiutil attach  http://download.asperasoft.com/download/sw/connect/3.6.6/AsperaConnectInstaller-3.6.6.119698.dmg
 # sudo installer -package /Volumes/Aspera\ Connect\ Installer/AsperaConnectInstaller.pkg -target /
 # sudo hdiutil detach /Volumes/Aspera\ Connect\ Installer/
-
+osascript <<EOD
 tell application "Aspera Connect"
 	activate
 	tell application "System Events"
@@ -17,3 +17,4 @@ tell application "Aspera Connect"
 		end tell
 	end tell
 end tell
+EOD
