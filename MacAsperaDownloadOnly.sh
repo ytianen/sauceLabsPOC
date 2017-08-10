17a1f7e006a7b51b6bc397b7f17d478c7ace36cb
iano <ian.oroceo@fox.com>
2017-08-10 14:23:02 -0700

    updating download mac applescript. adding log

--- MacAsperaDownloadOnly.sh
+++ MacAsperaDownloadOnly.sh
@@ -9,13 +9,27 @@ osascript <<EOD
                     end repeat
                     log
                     delay 5
-                    keystroke return
-                    delay 2
-                    log
-                    keystroke return
+                    key code 53
                 end tell
                 log
             end tell
     end tell
+        tell application "Aspera Connect"
+                activate
+                tell application "System Events"
+                    tell process "Aspera Connect"
+                        repeat until exists of button 1 of window 1
+                        delay 5
+                        end repeat
+                        log
+                        delay 5
+                        keystroke return
+                        delay 2
+                        log
+                        keystroke return
+                    end tell
+                    log
+                end tell
+        end tell
 EOD
 
