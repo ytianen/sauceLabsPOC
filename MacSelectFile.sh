7c62900c6e01f8cd6db108eb5dd321e3f59b2a98
newnewscat <amyf628@gmail.com>
2017-08-29 11:05:27 -0700

    Update MacSelectFile.sh

--- MacSelectFile.sh
+++ MacSelectFile.sh
@@ -4,29 +4,6 @@ MyValue=$1
 #osascript <<EOD
 /usr/bin/osascript<<EOF
     on run
-    processCheck()
-on processCheck()
-	tell application "System Events" to get name of every process
-	if the result contains "GridLauncher" then
-		
-	else
-		processCheck()
-	end if
-end processCheck
-#     	repeat until application "GridLauncher" is running
-#     		delay 2
-#     	end repeat
-     	tell application "Safari"
-		activate
-        tell application "System Events"
-		tell process "Safari"
-		repeat until exists of button "Trust" of sheet 1 of window 1
-			delay 1
-		end repeat
-    		click button "Trust" of sheet 1 of window 1
-	end tell
-	end tell
-	end tell
         tell application "Aspera Connect"
 	        activate
 	        set myFile to do shell script "echo '$MyValue'"
