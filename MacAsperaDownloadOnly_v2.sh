5fe01d7e035faed2d9b8ee85b1ef83393245600c
Tian Yu <ytianen@gmail.com>
2022-04-07 16:36:02 -0700

    Update MacAsperaDownloadOnly_v2.sh
    changing allow to yes for newer aspera version

--- MacAsperaDownloadOnly_v2.sh
+++ MacAsperaDownloadOnly_v2.sh
@@ -7,13 +7,13 @@ tell application "Aspera Connect"
 	activate
 	tell application "System Events"
 		tell process "Aspera Connect"
-			repeat until exists button "Allow" of window 1
+			repeat until exists button "Yes" of window 1
 				delay 1
 			end repeat
 			delay 1
 			click checkbox of window 1
 			delay 1
-			click button "Allow" of window 1
+			click button "Yes" of window 1
 		end tell
 	end tell
 end tell
